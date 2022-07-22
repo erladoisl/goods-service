c874717c9ee26bd09566962ab88eba4adb50ebb4
Rakhina Latypova <rakhina.bdd@gmail.com>
2022-07-22 14:38:31 +0300

    rf

--- parsing/management/commands/scrape.py
+++ parsing/management/commands/scrape.py
@@ -2,8 +2,7 @@ from parsing.parsing.firebase import update_prices
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    help = "collect jobs"   
+    help = "run parse goods price"   
 
     def handle(self, *args, **options):
         update_prices()
-
