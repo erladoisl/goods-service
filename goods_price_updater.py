973ebdf1f7edbfe96c742d8d9f5f4d69d07b7b9e
LR <erladoisl@gmail.com>
2022-08-19 08:44:06 +0300

    логирование времени выполнения скрипта

--- goods_price_updater.py
+++ goods_price_updater.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from modules.firebase import update_prices
 from modules.config import project_path
 import logging
@@ -5,4 +6,5 @@ import logging
 logging.basicConfig(filename=f'{project_path}dtp.log', level=logging.INFO)
 
 if __name__ == '__main__':
+    logging.info(f'{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}\nНачало процесса получения актуальных цен')
     update_prices()
