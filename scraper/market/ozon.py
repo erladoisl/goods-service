730f1b0a046b6927b459fae646db14943bbc6554
erladoisl <erladoisl@gmail.com>
2023-07-26 16:48:38 +0300

    br ozon parsing

--- scraper/market/ozon.py
+++ scraper/market/ozon.py
@@ -4,6 +4,7 @@ from pyquery import PyQuery
 from scraper.market.util import to_int, save_file
 import logging
 import config as c
+import re
 
 fh = logging.FileHandler(c.LOGGER_CONFIG['file'])
 fh.setFormatter(c.LOGGER_CONFIG['formatter'])
@@ -18,27 +19,37 @@ def parse(html):
         Возвращает стоимость товара из HTML
     '''
     price = -1
-    
+
     try:
         if for_sale(html):
-            pq = PyQuery(html)
-            price = to_int(pq('div[data-replace-layout-path="[4][0][2][0][1][0][1][0][0][1]"] span span')[-1].text.split('₽')[0])
+            res = re.search('<span class="sk0">([0-9]+\u2009){0,}₽', html)
+            log.debug(f're result: {res}')
+
+            if res:
+                price = int(
+                    re.search('(\d+\u2009){0,}(?=₽)', res[0])[0].replace('\u2009', ''))
     except:
         file_name = save_file('ozon_html', 'html', html)
         log.error(
             f'Ошибка получения стоимости товара, текст HTML будет сохранен в файл: {file_name}\n {traceback.format_exc()}')
     finally:
-        
+
         log.debug(f'Price: {price}')
-        
+
         return price
+
 
 def for_sale(html):
     '''
         Возвращает True если товар все еще продается. Иначе - False
     '''
-    log.info('Товар закончился в ozon')
-    return 'Этот товар закончился' not in html
+    res = 'Этот товар закончился' not in html
+
+    if not res:
+        log.info('Товар закончился в ozon')
+
+    return res
+
 
 if __name__ == '__main__':
     with open('C:/Users/User/Documents/goods_full/goods-service/modules/market/test/ozon.html', 'r', encoding="utf-8") as f:
