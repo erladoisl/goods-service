31cd499fcda6deec061ea23cd6768679c271add7
erladoisl <erladoisl@gmail.com>
2023-07-26 16:47:32 +0300

    default location

--- config.example.py
+++ config.example.py
@@ -10,3 +10,8 @@ DB_NAME = 'golden-eye.db'
 LOGGER_CONFIG = dict(level=logging.DEBUG,
                      file='app.log',
                      formatter=logging.Formatter('%(asctime)s [%(levelname)s] - %(name)s:%(message)s'))
+
+
+LOCATION_ACCURACY = 100
+DEFAULT_LATITUDE = 55.783963
+DEFAULT_LONGITUDE = 49.127415
