cf9708c2e0f745f11d8a1d6217e5fc20623ba75c
erladoisl <erladoisl@gmail.com>
2023-07-31 12:29:45 +0300

    selenium для определения загрузки страницы по названиям классов

--- config.example.py
+++ config.example.py
@@ -5,8 +5,6 @@ project_path = '/home/rakhina/goods-service/'
 chromedriver_path = f'{project_path}modules/chromedriver_windows.exe'
 firebase_admin_sdk_path = f'{project_path}goods-gazer-firebase-adminsdk.json'
 
-DB_NAME = 'golden-eye.db'
-
 LOGGER_CONFIG = dict(level=logging.DEBUG,
                      file='app.log',
                      formatter=logging.Formatter('%(asctime)s [%(levelname)s] - %(name)s:%(message)s'))
@@ -15,3 +13,9 @@ LOGGER_CONFIG = dict(level=logging.DEBUG,
 LOCATION_ACCURACY = 100
 DEFAULT_LATITUDE = 55.783963
 DEFAULT_LONGITUDE = 49.127415
+
+CLASS_NAME = {'www.eldorado.ru': 'headerRegionName', 
+              'sbermegamarket.ru': 'catalog-default', 
+              'www.dns-shop.ru': 'product-card-top', 
+              'market.yandex.ru': 'utilityfocus',
+              'www.ozon.ru': ''}
